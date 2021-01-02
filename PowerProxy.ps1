6856ebf8be85b9388d90fcdb115ddb35090d746a
GrimGram <57014148+Gr1mmie@users.noreply.github.com>
2021-01-01 21:55:09 -0500

    Update PowerProxy.ps1

--- PowerProxy.ps1
+++ PowerProxy.ps1
@@ -31,30 +31,30 @@ try{
     write-host "[*] Awaiting connection..." -fore cyan
     while (-not $svr.Pending()){
       if ($svr.Pending()){break}
-        start-sleep 1
+      start-sleep 1
     }
     $conn = $svr.AcceptTcpClient()
     if ($conn){
-    write-host "[*] Connection established" -fore green
-    $strm = $conn.GetStream()
-    $read = New-Object System.IO.StreamReader $strm
-    do{
+     write-host "[*] Connection established" -fore green
+     $strm = $conn.GetStream()
+     $read = New-Object System.IO.StreamReader $strm
+     do{
       	$line = $read.ReadLine()
       	write-host $line
       	if ($traffic){ $traffic = $traffic + "$line`n" }
       	else {$traffic = "$line`n"}
-    }while ($line)
-    Write-host "[*] Press f to forward, d to drop, or x to quit"
-    $ui = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
-    if ($ui.Character -eq "x"){exit}
-    elseif($ui.Character -eq "d"){
+     }while ($line)
+     Write-host "[*] Press f to forward, d to drop, or x to quit"
+     $ui = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
+     if ($ui.Character -eq "x"){exit}
+     elseif($ui.Character -eq "d"){
     	Write-host "[*] Dropping packet..."
 	start-sleep 1
     	if ($strm){$strm.Dispose()}
     	if ($read){$read.Dispose()}
     	if ($conn){$conn.close()}
-    }
-    elseif($ui.Character -eq "f"){
+     }
+     elseif($ui.Character -eq "f"){
     	if ($ip_addr -eq '0.0.0.0'){$ip_addr = '127.0.0.1'}
 		#open 9080 w/ nc to test
 		$port = 9080
@@ -66,13 +66,13 @@ try{
 		write-host "[-] Invalid input detected" -fore red
 		exit
 	}
-    }
-    start-sleep 0.5
-    if ($traffic){Clear-Variable -name traffic}
-    if ($strm){$strm.Dispose()}
-    if ($read){$read.Dispose()}
-    if ($conn){$conn.close()}
-    }
+     }
+     start-sleep 0.5
+     if ($traffic){Clear-Variable -name traffic}
+     if ($strm){$strm.Dispose()}
+     if ($read){$read.Dispose()}
+     if ($conn){$conn.close()}
+   }
 }
 catch [System.Net.Sockets.SocketException]{Write-host "[-] sumthing borked`n[-] svr still open" -fore red}
 catch {
